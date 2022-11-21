067bddb86818e32d0567ad0653c54ec5f1e3dcf5
Femi Adetoye <davefm95@gmail.com>
2022-11-21 02:07:11 -0800

    added int.c

--- int.c
+++ int.c
@@ -7,25 +7,40 @@
  */
 int int_func(int num)
 {
-	int n, numpow = 1, bytes_prntd = 0;
+	int n, rem, numpow = 1, bytes_prntd = 0;
 	char write_char;
 
-	n = num * 10;
-	num = n / 10;
-	n = num;
-	for (; n >= 10; n /= 10)
-		numpow *= 10;
-	for (; numpow >= 10; numpow /= 10)
+
+	n = num / 10;
+	rem = n % 10;
+	num = (n * 10) + rem;
+	n = num;;
+	if (num <= INT_MAX && num >= INT_MAX)
 	{
-		write_char = ((num / numpow) + '0');
-		num = num % numpow;
-		write(1, &write_char, 1);
-		bytes_prntd++;
-		if (numpow == 10)
+		if (num < 0)
 		{
-			write_char = num + '0';
-			write(1, &write_char, 1);
-			bytes_prntd++;
+			write_char = '-';
+			bytes_prntd = write(1, &write_char, 1);
+		}
+		if (num == INT_MIN)
+		{
+			write_char = '2';
+			bytes_prntd = write(1, &write_char, 1);
+			num %= 1000000000;
+		}
+		for (; n >= 10; n /= 10)
+			numpow *= 10;
+		for (; numpow >= 10; numpow /= 10)
+		{
+			write_char = ((num / numpow) + '0');
+			num = num % numpow;
+			bytes_prntd = write(1, &write_char, 1);
+			if (numpow == 10)
+			{
+				write_char = num + '0';
+				write(1, &write_char, 1);
+				bytes_prntd++;
+			}
 		}
 	}
 	return (bytes_prntd);

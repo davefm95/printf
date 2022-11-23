609a6f4a894e6121ef690802e39944aa4d75a39e
Femi Adetoye <davefm95@gmail.com>
2022-11-23 09:12:38 -0800

    add

--- _printf.c
+++ _printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 		}
 		if (format[i] != '%')
 			bytes_prntd += write(1, &format[i], 1);
-		if (format[i] == '%' && format[i + 1] != '\0')
+		else if (format[i] == '%' && format[i + 1] != '\0')
 			conv_charswitch++;
 		else if (format[i + 1] == '\0')
 			return (-1);
